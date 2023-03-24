1c5177f1d5b47d47b3b892c4bedd8e128b9ea955
DiogoRodrigues <diogo.rodrigues@itsector.pt>
2023-03-24 11:31:38 +0000

    Credential handling

--- website/src/pages/Login/LoginForm.js
+++ website/src/pages/Login/LoginForm.js
@@ -12,30 +12,57 @@ function LoginForm({ handleLogin }) {
   const handleSubmit = async (event) => {
     const data = {email,password}
     event.preventDefault();
-    await fetch("http://localhost:4000/users/authenticate", {
+    await fetch("http://localhost:5000/users/authenticate", {
       method: 'POST',
       headers: {
         "Content-type": "application/json; charset=UTF-8"
       },
       body: JSON.stringify(data)
     }).then(response => {
-      handleLogin();
-      return response.json()
-    }).then(
-      (result) => {
-        console.log("Hello " + result.firstName + " " + result.lastName + "!");
-        localStorage.setItem('user', JSON.stringify(result))
+      if (!response.ok) {
+        throw new Error("Wrong credentials");  
       }
-    ).catch(err => {
-      console.log(err)
-      setError(err);
-    })
+      return response.json();
+    }).then(data =>{
+      handleLogin();
+      console.log("Hello " + data.firstName + " " + data.lastName + "!");
+      localStorage.setItem('user', JSON.stringify(data))
+    }).catch(error => {
+      setError(error);
+    });
+    
   };
-
+  if (!error) {
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
   return (
     <form onSubmit={handleSubmit}>
       <label>
-        Emaaaaaaaaaaaaaaaaaaaaaaail:
+        Email:
         <input
           type="email"
           value={email}
@@ -54,7 +81,7 @@ function LoginForm({ handleLogin }) {
         />
       </label>
       <br />
-      {error && <div style={{ color: "red" }}>{error}</div>}
+      <div style={{ color: "red" }}>Wrong credentials!</div>
       <button type="submit">Login</button>
     </form>
   );
