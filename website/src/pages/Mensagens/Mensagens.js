84b5242bdaf54242ec7a4a99e2a5e2be7f0d37d6
joao12520 <joaotomas12520@gmail.com>
2023-05-30 11:02:10 +0100

    Messaging fetch

--- website/src/pages/Mensagens/Mensagens.js
+++ website/src/pages/Mensagens/Mensagens.js
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Mensagens.css';
 
-const tabelaDados = [
-  { date: 112, message: "sdafsdafsdafasdfsda" },
-  { date: 2321321, message: "asfsdafsdasdafsadf" },
-  { date: 321321, message: "asdfsdafdfadfds" },
-  { date: 213214, message: "asdfafasfasdfsdafs" },
-];
-
 const App = () => {
+  const [history, setHistory] = useState([]);
+
+  useEffect(() => {
+    fetchHistory();
+  }, []);
+
+  const fetchHistory = async () => {
+    const user = localStorage.getItem('user');
+    const token = JSON.parse(user).Token;
+
+    try {
+      await fetch('http://localhost:4003/history', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ token })
+      }).then(response => {
+        if (!response.ok) {
+          throw new Error('There was an error');
+        }
+        return response.json();
+      }).then(responsedata => {
+        setHistory(responsedata);
+      })
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="container">
       <h1 className="title">Messaging - asynchronous exchange of messages</h1>
       <table className="table">
         <thead>
           <tr>
+            <th>Type</th>
             <th>Date</th>
-            <th>Message</th>
           </tr>
         </thead>
         <tbody>
-          {tabelaDados.map((item) => (
-            <tr key={item.id}>
+          {history.map((item, index) => (
+            <tr key={index}>
+              <td>{item.type}</td>
               <td>{item.date}</td>
-              <td>{item.message}</td>
             </tr>
           ))}
         </tbody>
